eb0686dd17f8784126c1836394e03d175a40f4d7
Jiří Velek <bleksak@protonmail.com>
2023-07-21 13:10:57 +0200

    header and footer

--- assets/react/screens/Home.tsx
+++ assets/react/screens/Home.tsx
@@ -52,12 +52,12 @@ function Logo() {
     const [t, _] = useTranslation();
 
     return <>
-        <div className="main">
-            <div className="col title">
+        <header>
+            <div>
                 <h1>{t('title').toUpperCase()}</h1>
                 <h2>{t('join_us')}</h2>
             </div>
-        </div>
+        </header>
     </>
 }
 
