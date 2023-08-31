d74bb0c3da720e3ded81d8bce98f4101ba45b420
Jiří Velek <bleksakcz@gmail.com>
2023-08-31 20:01:07 +0200

    add function to retrieve rejected messages

--- src/Repository/SubmissionRepository.php
+++ src/Repository/SubmissionRepository.php
@@ -47,7 +47,10 @@ class SubmissionRepository extends ServiceEntityRepository
         $query = $this->createQueryBuilder('s')
             ->select('s')
             ->where('s.user = :userId')
+            ->andWhere('s.accepted = :accepted')
+
             ->setParameter('userId', $user->getId())
+            ->setParameter('accepted', true)
         ;
 
         $paginator = new Paginator($query);
