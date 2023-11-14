3d6e6c3729a5a1a4af141075a8ea850f2e43b8f8
Jiri Velek <jiri.velek7@protonmail.com>
2023-11-14 16:08:29 +0100

    Add ApiResource for caching

--- src/Controller/ApiResource/CacheController.php
+++ src/Controller/ApiResource/CacheController.php
@@ -3,12 +3,14 @@
 namespace App\Controller\ApiResource;
 
 use App\Action\CacheActions;
+use App\Attributes\ApiResource;
 use App\Attributes\ApiRoute;
 use App\Entity\Season;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[ApiResource('Cache')]
 class CacheController extends AbstractController
 {
     public function __construct(private readonly CacheActions $action)
