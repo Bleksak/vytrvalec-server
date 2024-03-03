3324fc5055c595a87b21412b9c8fe651c8ed1c4f
Jiri Velek <jiri.velek7@protonmail.com>
2024-03-03 23:06:45 +0100

    WeeklyElevationExtraPoints - fix: previously was calculated as daily elevation

--- src/CustomLogic/WeeklyElevationExtraPoints.php
+++ src/CustomLogic/WeeklyElevationExtraPoints.php
@@ -26,11 +26,11 @@ class WeeklyElevationExtraPoints implements ExtraPoints
         $query = $this->entityManagerInterface->getConnection()->prepare('
             WITH
                 sub AS (
-                    SELECT SUM(s.elevation) as value, a.min_elevation, s.activity_id as activity_id, s.user_id as user_id, s.date
+                    SELECT SUM(s.elevation) as value, a.min_elevation s.activity_id as activity_id, s.user_id as user_id
                     FROM submission s
                     INNER JOIN activity a ON s.activity_id = a.id
                     WHERE s.week = ? AND s.accepted = ? AND s.season_id = ?
-                    GROUP BY s.date, s.user_id, s.activity_id
+                    GROUP BY s.user_id, s.activity_id
                     HAVING(value) >= min_elevation
                 ),
                 sorted AS (
