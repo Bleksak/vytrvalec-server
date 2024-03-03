475e6a8a4eb4eecba47f35a6ad6b178234d5d024
Jiri Velek <jiri.velek7@protonmail.com>
2024-03-03 23:24:22 +0100

    WeeklyElevationExtraPoints - fix typo

--- src/CustomLogic/WeeklyElevationExtraPoints.php
+++ src/CustomLogic/WeeklyElevationExtraPoints.php
@@ -26,7 +26,7 @@ class WeeklyElevationExtraPoints implements ExtraPoints
         $query = $this->entityManagerInterface->getConnection()->prepare('
             WITH
                 sub AS (
-                    SELECT SUM(s.elevation) as value, a.min_elevation s.activity_id as activity_id, s.user_id as user_id
+                    SELECT SUM(s.elevation) as value, a.min_elevation, s.activity_id as activity_id, s.user_id as user_id
                     FROM submission s
                     INNER JOIN activity a ON s.activity_id = a.id
                     WHERE s.week = ? AND s.accepted = ? AND s.season_id = ?
