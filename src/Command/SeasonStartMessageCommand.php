d77dc1c487272277f3da3b4dd2d997ff51d451d5
Jiri Velek <bleksak@protonmail.com>
2025-03-31 15:49:21 +0200

    add season start message command, just in case it fails

---
+++ src/Command/SeasonStartMessageCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Messages\SeasonStartMessage;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+#[AsCommand(
+    name: 'season:start-message',
+    description: 'Add a short description for your command',
+)]
+final class SeasonStartMessageCommand extends Command
+{
+    public function __construct(
+        private MessageBusInterface $messageBus,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('season_id', InputArgument::REQUIRED, 'Season ID to resend the mails')
+            ->addOption('send', 's', InputOption::VALUE_NONE, 'if you really want to send the emails, you must provide this')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $send = $input->getOption('send');
+
+        if (!$send) {
+            return -1;
+        }
+
+        $seasonId = $input->getArgument('season_id');
+
+        if (!filter_var($seasonId, FILTER_VALIDATE_INT)) {
+            return -1;
+        }
+
+        $seasonId = (int) $seasonId;
+
+        $this->messageBus->dispatch(
+            new SeasonStartMessage($seasonId)
+        );
+
+        return Command::SUCCESS;
+    }
+}
