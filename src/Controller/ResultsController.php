fe932156ea5cbb993bc5f07c973323656962dd9c
Jiri Velek <jiri.velek@yahoo.com>
2025-12-06 23:22:08 +0100

    optimize more queries, fix issues in results and rules controller

--- src/Controller/ResultsController.php
+++ src/Controller/ResultsController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\FacultyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,10 +15,14 @@ final class ResultsController extends AbstractController
 {
     public const string ROUTE = 'results';
 
-    public function __construct() {}
+    public function __construct(
+        private FacultyRepository $facultyRepository,
+    ) {}
 
     public function __invoke(): Response
     {
-        return $this->render('results.html.twig');
+        return $this->render('results.html.twig', [
+            'faculties' => $this->facultyRepository->findAllWithTranslations(),
+        ]);
     }
 }
--- src/Controller/RulesController.php
+++ src/Controller/RulesController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\FacultyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,8 +15,14 @@ final class RulesController extends AbstractController
 {
     public const string ROUTE = 'rules';
 
+    public function __construct(
+        private FacultyRepository $facultyRepository,
+    ) {}
+
     public function __invoke(): Response
     {
-        return $this->render('rules.html.twig');
+        return $this->render('rules.html.twig', [
+            'faculties' => $this->facultyRepository->findAllWithTranslations(),
+        ]);
     }
 }
