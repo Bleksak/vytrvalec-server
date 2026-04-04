4e0352512a4b78ed11abd59993893a414fce51d1
Jiri Velek <jiri.velek@yahoo.com>
2026-04-04 17:35:37 +0200

    prepare behat for test suite

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tests;
+namespace App\Tests;
 
 use Symfony\Component\Dotenv\Dotenv;
 
@@ -15,5 +15,5 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
     require dirname(__DIR__) . '/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    new Dotenv()->bootEnv(dirname(__DIR__) . '/.env');
+    new Dotenv()->bootEnv(dirname(__DIR__) . '/.env.test');
 }
